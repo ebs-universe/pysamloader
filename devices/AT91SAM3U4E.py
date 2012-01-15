b0f784e60f784d96c4febd1a26d21172069cd832
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2012-01-15 09:56:06 +0530

    License Texts cleaned up

--- devices/AT91SAM3U4E.py
+++ devices/AT91SAM3U4E.py
@@ -1,5 +1,21 @@
+# Copyright (c) 2012 Chintalagiri Shashank
+#
+# This file is part of pysamloader.
 
-class AT91SAM7X512(SAMDevice):
+# pysamloader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pysamloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pysamloader.  If not, see <http://www.gnu.org/licenses/>.
+
+class AT91SAM7X512(object):
     EFC_FCR = '400E0804'
     EFC_FSR = '400E0808'
     AutoBaud = False
@@ -10,4 +26,4 @@ class AT91SAM7X512(SAMDevice):
     FS_ADDRESS = '00080000'
     PAGE_SIZE = 256
     def __init__(self, args):
-        SAMDevice.__init__(self)
+        pass
--- devices/AT91SAM7X512.py
+++ devices/AT91SAM7X512.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2012 Chintalagiri Shashank
+#
+# This file is part of pysamloader.
+
+# pysamloader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pysamloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pysamloader.  If not, see <http://www.gnu.org/licenses/>.
 
 class AT91SAM7X512(object):
     EFC_FCR = 'FFFFFF64'
