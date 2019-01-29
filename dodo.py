a187a3efdf578af9e94af2d00a1f1321c78691a5
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-01-29 18:54:06 +0530

    Minor cleanup of build scripts.

--- dodo.py
+++ dodo.py
@@ -10,18 +10,6 @@ from pkg_resources import get_distribution
 SCRIPT_NAME = 'pysamloader'
 SCRIPT_VERSION = get_distribution(SCRIPT_NAME).version
 
-if platform.system() == 'Linux':
-    import tarfile
-    package_ext = '.tar.gz'
-    executable_ext = ''
-elif platform.system() == 'Windows':
-    import zipfile
-    package_ext = '.zip'
-    executable_ext = '.exe'
-else:
-    raise NotImplementedError("Platform not supported : {0}"
-                              "".format(platform.system()))
-
 
 def _get_python_shared_lib():
     real_exec = sys.executable
@@ -30,6 +18,21 @@ def _get_python_shared_lib():
     return os.path.normpath(
         os.path.join(os.path.split(real_exec)[0], os.pardir, 'lib')
     )
+
+
+if platform.system() == 'Linux':
+    import tarfile
+    package_ext = '.tar.gz'
+    executable_ext = ''
+    pyi_prefix = "LD_LIBRARY_PATH={0} ".format(_get_python_shared_lib())
+elif platform.system() == 'Windows':
+    import zipfile
+    package_ext = '.zip'
+    executable_ext = '.exe'
+    pyi_prefix = ""
+else:
+    raise NotImplementedError("Platform not supported : {0}"
+                              "".format(platform.system()))
 
 
 def _base_folder():
@@ -53,63 +56,15 @@ def _executable_path():
     return os.path.join(_binary_dist_folder(), _executable_name())
 
 
-def task_build_binary():
-    if platform.system() == 'Linux':
-        prefix = "LD_LIBRARY_PATH={0} ".format(_get_python_shared_lib())
-    else:
-        prefix = ""
-    return {
-        'actions': [prefix + 'pyinstaller pysamloader.spec'],
-        'targets': [_executable_path()]
-    }
-
-
-def _package_name():
+def _binary_package_name():
     return "{0}-{1}-{2}-{3}{4}".format(
         SCRIPT_NAME, SCRIPT_VERSION,
         platform.system().lower(), platform.machine(), package_ext
     )
 
 
-def _package_path():
-    return os.path.join(_binary_dist_folder(), _package_name())
-
-
-def _create_package():
-    package_content = [
-        (os.path.join(_binary_dist_folder(), _executable_name()), _executable_name()),
-        (os.path.join(_base_folder(), 'LICENSE'), 'LICENSE'),
-        (os.path.join(_base_folder(), 'README.rst'), 'README.rst'),
-        (os.path.join(_base_folder(), 'pysamloader', 'devices'), 'devices'),
-    ]
-
-    def _filter_py(tarinfo):
-        if os.path.splitext(tarinfo.name)[1] == '.pyc':
-            return None
-        if tarinfo.isdir() and tarinfo.name.endswith('__pycache__'):
-            return None
-        return tarinfo
-
-    if platform.system() == 'Linux':
-        with tarfile.open(_package_path(), "w:gz") as tar:
-            for item, arc in package_content:
-                tar.add(item, arcname=arc, filter=_filter_py)
-    elif platform.system() == 'Windows':
-        pass
-
-
-def task_package_binary():
-    return {
-        'task_dep': ['build_binary'],
-        'actions': [_create_package],
-        'targets': [_package_path()]
-    }
-
-
-def task_publish_binary():
-    return {
-        'actions': []
-    }
+def _binary_package_path():
+    return os.path.join(_binary_dist_folder(), _binary_package_name())
 
 
 def _sdist_name():
@@ -126,6 +81,51 @@ def _bdist_name():
     return '{0}-{1}-{2}-none-any.whl'.format(SCRIPT_NAME, SCRIPT_VERSION, pytag)
 
 
+def task_build_binary():
+    return {
+        'actions': [pyi_prefix + 'pyinstaller {0}.spec'.format(SCRIPT_NAME)],
+        'targets': [_executable_path()]
+    }
+
+
+def _create_binary_package():
+    package_content = [
+        (os.path.join(_binary_dist_folder(), _executable_name()), _executable_name()),
+        (os.path.join(_base_folder(), 'LICENSE'), 'LICENSE'),
+        (os.path.join(_base_folder(), 'README.rst'), 'README.rst'),
+        (os.path.join(_base_folder(), 'pysamloader', 'devices'), 'devices'),
+    ]
+
+    def _filter_py(tarinfo):
+        if os.path.splitext(tarinfo.name)[1] == '.pyc':
+            return None
+        if tarinfo.isdir() and tarinfo.name.endswith('__pycache__'):
+            return None
+        return tarinfo
+
+    if platform.system() == 'Linux':
+        with tarfile.open(_binary_package_path(), "w:gz") as tar:
+            for item, arc in package_content:
+                tar.add(item, arcname=arc, filter=_filter_py)
+    elif platform.system() == 'Windows':
+        pass
+
+
+def task_package_binary():
+    return {
+        'task_dep': ['build_binary'],
+        'actions': [_create_binary_package],
+        'targets': [_binary_package_path()]
+    }
+
+
+def task_publish_binary():
+    return {
+        'actions': [],
+        'file_deps': [_binary_package_path()],
+    }
+
+
 def task_build_pypi():
     return {
         'actions': ['python setup.py sdist bdist_wheel'],
@@ -137,6 +137,10 @@ def task_build_pypi():
 
 
 def task_publish_pypi():
+    if 'dev' in SCRIPT_VERSION:
+        return {
+            'actions': []
+        }
     return {
         'file_dep': [
             os.path.join(_dist_folder(), _sdist_name()),
